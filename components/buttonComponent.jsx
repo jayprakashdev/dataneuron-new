3dd29f264440031573e3234d5cf8f4df91f83e9a
amitprecily13 <amit.shaw@precily.com>
2021-09-04 14:00:33 +0530

    created about pricay and resource pages

---
+++ components/buttonComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const ButtonComponent = (props) => {
+    const {icon, src, text, filled}=props;
+    return (
+        <div 
+        className="flex py-2 px-4 rounded-3xl bg-white justify-center" 
+        style={
+            {border:"1px solid #0000FF",
+             background:`${filled?"#0000FF":"#FFF"}`
+             , color:`${filled?"#FFF":"#0000FF"}`}
+             }
+        >
+            <div className="flex">
+                {icon&&<img src={src} alt="icon" className="mr-3"/>}
+                <p>{text}</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default ButtonComponent;
